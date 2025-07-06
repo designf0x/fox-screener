be39ab6d9b43fe500c412ffe257d357e2e6b7e86
designf0x <elbikbulatoff@gmail.com>
2025-07-06 15:24:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,4 +76,8 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()
+    asyncio.get_event_loop().create_task(main())
+    asyncio.get_event_loop().run_forever()
+
